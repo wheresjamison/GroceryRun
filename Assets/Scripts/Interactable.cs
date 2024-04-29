97c81b5aca8eb03f9edd8e0b0e5c786389d3b4c3
Albert Jamison Legaspi <legaspijamison@gmail.com>
2024-04-28 23:24:21 -0700

    Initial commit

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Interactable : MonoBehaviour
+{
+    //here is an example of the template method pattern.
+    public string promptMessage;
+
+    public void BaseInteract()
+    {
+        Interact();
+    }
+
+    protected virtual void Interact()
+    {
+        //no code here, just a template function to be written in by subclasses
+    }
+    
+}
