1f20c21a73a23acd7b9eb9e1096df2d043b0b4b6
Albert Jamison Legaspi <legaspijamison@gmail.com>
2024-05-29 05:04:01 -0700

    nice work

--- Assets/Scripts/PlayerRelated/PlayerLook.cs
+++ Assets/Scripts/PlayerRelated/PlayerLook.cs
@@ -8,15 +8,15 @@ public class PlayerLook : MonoBehaviour
     public Camera cam;
     public float xRotation = 0f;
 
-    public float xSensitivity = 30f;
-    public float ySensitivity = 30f;
-    
+    public float xSensitivity = .5f;
+    public float ySensitivity = .5f;
+
     public void ProcessLook(Vector2 input)
     {
         float mouseX = input.x;
         float mouseY = input.y;
         //camera rotation
-        xRotation -= (mouseY * Time.deltaTime) *ySensitivity;
+        xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
         //apply to camera transform
         cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
